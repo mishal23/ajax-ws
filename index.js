cd899cea91dc06218007b8e7ceeeec4abdca9b38
mishal23 <shahmishal1998@gmail.com>
2020-09-12 17:22:03 +0530

    code built during the hackathon

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 var app = express();
 
+const server = http.createServer(app);
+const io = socketio(server);
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function(req, res){
+io.on('connection', function(socket){
+
+    socket.on('silly-hacks', function(data){
+        socket.emit('resp', data);
+    });
+
+    // socket.emit: emit to the client
+    // io.emit: emit to everyone on the systen
+    // socket.broadcast.emit: emit to everyone on the system except for the client
+});
+
+app.get('/ajax', function(req, res){
+    res.send("GET request received");
+});
+
+server.listen(3000, function(req, res){
     console.log("Server listening on port 3000");
 });
--- public/app.js
+++ public/app.js
@@ -6,6 +6,45 @@ $(function() {
     
     var ajax_total = 0;
     var sockets_total = 0;
+
+    $("#submitRequests").click(function(){
+      var numOfRequests = $("#requests").val();
+      console.log(numOfRequests);
+
+      doGET(numOfRequests);
+
+      setTimeout(function(){}, 5000);
+
+      doSocket(numOfRequests);
+
+      setTimeout(drawGraph, 7000);
+    });
+
+    function doGET(numOfRequests){
+      var ajax_start = new Date().getTime();
+      for(var i=0;i<numOfRequests;i++){
+        $.get("/ajax", function(data, status){
+          var ajax_elapsed = new Date().getTime() - ajax_start;
+          ajax_total = ajax_elapsed;
+        });
+      }
+    }
+
+    function doSocket(numOfRequests){
+      var socket_start = new Date().getTime();
+      var socket = io();
+      for(var i=0;i<numOfRequests;i++){
+        socket.emit('silly-hacks', i);
+      }
+
+      socket.on('resp', function(data){
+        if(data == numOfRequests -1){
+          var webs_elapsed = new Date().getTime() - socket_start;
+          sockets_total = webs_elapsed;
+        }
+      })
+    }
+    
     
     function drawGraph(){
         var data = [
